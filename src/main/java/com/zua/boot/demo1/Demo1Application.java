e47c0cc97cb5a4ab98f92f3374a3cb1fcb57d6fa
gx <1102355296@qq.com>
2024-03-12 20:21:00 +0800

    'Test2'

--- src/main/java/com/zua/boot/demo1/Demo1Application.java
+++ src/main/java/com/zua/boot/demo1/Demo1Application.java
@@ -2,7 +2,7 @@ package com.zua.boot.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// gx
 @SpringBootApplication
 public class Demo1Application {
 
